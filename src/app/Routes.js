6ff7cb28f90f82b82bbbe98767898092208e6347
Sergey <grayseneov@gmail.com>
2019-02-11 00:19:06 +0300

    remove app folder

--- src/app/Routes.js
+++
@@ -1,7 +0,0 @@
-import { Switch, Route } from 'react-router-dom';
-
-export default () => (
-  <Switch>
-    <Route exact path="/" render={() => <div>/root</div>} />
-  </Switch>
-);
--- src/app/utils/createRootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
-
-export default (history, asyncReducers = {}) => combineReducers({
-  router: connectRouter(history),
-  ...asyncReducers,
-});
